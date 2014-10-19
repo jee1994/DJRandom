6564921c9f1f010b262a1bc7bdfd525b596af5d5
CLOUD <CLOUD@CLOUD-PC>
2014-10-18 23:05:01 -0500

    check

--- DJRandom/src/com/example/djrandom/MainActivity.java
+++ DJRandom/src/com/example/djrandom/MainActivity.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-/*
+/* git commit 하는 법 
  * 어떤 파일 코드나 코멘트 변경후 저장 -> 그 파일이나 프로젝트 폴더 오른쪽 클릭 -> team -> 인덱스 추가(Add to Index) -> Git 디렉토리에서 -> 프로젝트 폴더 오른쪽 클릭 -> commit -> 메시지 쓴후 commit and push. 
- * 프로젝트
+ * 
+ * 프로젝트 원격 서버에서 가져오기 
+ * git 디렉토리 -> 오른쪽 클릭 -> pull 
  * 
  */
 public class MainActivity extends Activity {
