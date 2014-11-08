43c3c947dc808e57111b9867cfd75de04638bb4d
Jason Hahm <hahm95@hotmail.com>
2014-11-08 15:38:14 -0600

    button layout / bgcolor

--- DJRandom/gen/com/example/djrandom/R.java
+++ DJRandom/gen/com/example/djrandom/R.java
@@ -24,13 +24,21 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080006;
-        public static final int button=0x7f080000;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int button3=0x7f080003;
-        public static final int button4=0x7f080004;
-        public static final int button5=0x7f080005;
+        public static final int action_settings=0x7f08000e;
+        public static final int button=0x7f080003;
+        public static final int button1=0x7f080005;
+        public static final int button2=0x7f080007;
+        public static final int button3=0x7f080009;
+        public static final int button4=0x7f08000b;
+        public static final int button5=0x7f08000d;
+        public static final int title=0x7f080001;
+        public static final int wrapper=0x7f080002;
+        public static final int wrapper0=0x7f080000;
+        public static final int wrapper1=0x7f080004;
+        public static final int wrapper2=0x7f080006;
+        public static final int wrapper3=0x7f080008;
+        public static final int wrapper4=0x7f08000a;
+        public static final int wrapper5=0x7f08000c;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
